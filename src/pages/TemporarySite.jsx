9fcbdbd0e8a7d5822128624fccb3f035cdf5c333
Yash Hadade <yashhadade13@gmail.com>
2024-10-17 17:57:38 +0530

    some Data Tabel Changes Try MUI

--- src/pages/TemporarySite.jsx
+++ src/pages/TemporarySite.jsx
@@ -1,14 +1,10 @@
 
 import { deleteProduct } from "./DeleteAPI";
 import useCustomReactQuery from "./getForAllApi";
-
+import{TableContainer,Table,TableHead,TableBody,TableRow,TableCell,Paper,}from '@mui/material'
 
 const TemporarySite = () => {
   const [product,setProduct] = useCustomReactQuery('temp-site');
-
-  
- 
-  
 
   return (
     <div className="container">
@@ -16,7 +12,51 @@ const TemporarySite = () => {
         <h3>SITE LIST</h3>
         <button id="exportButton">Export to Excel</button>
         <h2>The Number of sites: {product.length}</h2>
-        <table className="table">
+          <TableContainer component={Paper}>
+            <Table aria-label='simple table'>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Sr no</TableCell>
+                  <TableCell>Client Name</TableCell>
+                  <TableCell>Site Name</TableCell>
+                  <TableCell>Branch Name</TableCell>
+                  <TableCell>Zone</TableCell>
+                  <TableCell>State</TableCell>
+                  <TableCell>City</TableCell>
+                  <TableCell>Address</TableCell>
+                  <TableCell>Actions</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+              {product.map((site, index) => (
+              <TableRow key={site.id}>
+                <TableCell>{index + 1}</TableCell>
+                <TableCell>{site.client.clientName}</TableCell>
+                <TableCell>{site.siteName}</TableCell>
+                <TableCell>{site.branch}</TableCell>
+                <TableCell>{site.zone.zoneName}</TableCell>
+                <TableCell>{site.state.stateName}</TableCell>
+                <TableCell>{site.city.cityName}</TableCell>
+                <TableCell>{site.address}</TableCell>
+                <TableCell>
+                  <button
+                    className="edit"
+                  >
+                    Edit
+                  </button>
+                  <button
+                   
+                    className="delete" onClick={() => deleteProduct("temp-site/delete", site.id, setProduct)}
+                  >
+                    Delete
+                  </button>
+                </TableCell>
+              </TableRow>
+            ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        {/* <table className="table">
           <thead>
             <tr>
               <th>Sr no</th>
@@ -57,7 +97,7 @@ const TemporarySite = () => {
               </tr>
             ))}
           </tbody>
-        </table>
+        </table> */}
       </div>
      
     </div>
