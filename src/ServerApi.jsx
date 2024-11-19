6f40a1b30a35677a678d0fb5ff17e831f5b08780
Yash Hadade <yashhadade13@gmail.com>
2024-11-19 13:28:47 +0530

    project is build

--- src/ServerApi.jsx
+++ src/ServerApi.jsx
@@ -1,6 +1,6 @@
 
 function ServerApi(){
-    const api="http://smartfm.ismartfacitechpl.com/feedback"
+    const api="https://smartfm.ismartfacitechpl.com/feedback/index.php?url="
     return api
 }
 export default ServerApi;
