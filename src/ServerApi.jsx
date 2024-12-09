654324e87cf47d4ebf8b81edbc12825d2ee8dc45
unknown <yashhadade13@gmai.com>
2024-12-09 16:22:07 +0530

    server api error solved

--- src/ServerApi.jsx
+++ src/ServerApi.jsx
@@ -1,10 +1,7 @@
 
 function ServerApi(){
     const api="https://smartfm.ismartfacitechpl.com/feedback/index.php?url="
-<<<<<<< HEAD
-=======
     
->>>>>>> target-branch
     return api
 }
 export default ServerApi;
